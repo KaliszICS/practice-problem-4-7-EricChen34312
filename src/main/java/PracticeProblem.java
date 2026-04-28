d9b5fcbe43a6daf24693051910ea274f89d174b6
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-04-28 13:47:07 +0000

    ssssssssssssssssssssssss

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,44 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validateAge(int age){
+		if(age < 0){
+			throw new IllegalArgumentException("Age cannot be negative");
+		} else if (age > 150){
+			throw new IllegalArgumentException("Age must be realistic");
+		}
+
+		return true;
+	}
+
+	public static double withdraw(double amount, double balance){
+		if(amount > balance){
+			throw new IllegalStateException("fund balance");
+		} else if (amount <= 0){
+			throw new IllegalArgumentException("negative");
+		}
+		return balance-amount;
+	}
+
+	public static int getElement(int[] array, int index){
+		if(array == null){
+			throw new NullPointerException("Array cannot be null");
+		} else if (index >= array.length){
+			throw new IndexOutOfBoundsException("I didn't need to do this but ok");
+		}
+		return array[index];
+	}
+
+	public static String validateEmail(String email){
+		if(email == null || email.equals("")){
+			throw new IllegalArgumentException("Email cannot be empty");
+		} else if (!email.contains("@")){
+			throw new IllegalArgumentException("Invalid email format");
+		} else if (!email.substring(email.indexOf("@")).contains(".")){
+			throw new IllegalArgumentException("Invalid email format");
+		}
+
+		return email.trim();
+	}
 
 }
